8707c250e1b105c24d03c0d9f95f5fb7836619b6
Glitch (glitch-hello-website) <none>
2023-07-07 15:30:49 +0000

    🚘🌅 Checkpoint ./styles.css:50750112/2093 ./script.js:50750112/4321 ./index.html:50750112/222

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-function changeDiv(i){
-  bol1 = document.get
-  if()
+function changeDiv(i) {
+    var listPage = document.getElementById("bol1")
+    var infoPage = document.getElementById("bol2")
+    var imgList = 
+    ["https://media.discordapp.net/attachments/1083303057544200272/1116713534156443718/8v2.png?width=1194&height=682",
+        "https://media.discordapp.net/attachments/1083303057544200272/1116708462680883271/A3.png?width=1194&height=682",
+        "http://127.0.0.1:5501/mobil/images/recommended4.jpg"]
+    var infoList = [
+      "Burada 1. kilim gösteriliyor",
+      "Burada 2. kilim gösteriliyor",
+      "Burada 3. kilim gösteriliyor"
+    ]
+    if (listPage.style.display = "unset") {
+        var image = document.getElementById("infoImage")
+        infoPage.style.display = "unset"
+        listPage.style.display = "none"
+        image.src = imgList[i];
+        infoList[i];
+    }
 }
