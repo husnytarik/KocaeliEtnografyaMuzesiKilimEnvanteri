50a9fd81b039f371277449bfcc4df4fcd7493bf0
Glitch (glitch-hello-website) <none>
2023-07-10 18:51:28 +0000

    🤗🚩 Checkpoint ./index.html:50750112/16002 ./script.js:50750112/25063 ./styles.css:50750112/7540

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
+// Handles loading the events for <model-viewer>'s slotted progress bar
+const onProgress = (event) => {
+  const progressBar = event.target.querySelector('.progress-bar');
+  const updatingBar = event.target.querySelector('.update-bar');
+  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+  if (event.detail.totalProgress === 1) {
+    progressBar.classList.add('hide');
+    event.target.removeEventListener('progress', onProgress);
+  } else {
+    progressBar.classList.remove('hide');
+  }
+};
+document.querySelector('model-viewer').addEventListener('progress', onProgress);
+
+
+//var modelvw = document.querySelector("model-viewer#kilim");
+//var model = modelvw.model;
+//console.log(model)
+
 function changeDiv(i) {
   var listPage = document.getElementById("bol1");
   var infoPage = document.getElementById("bol2");
   var imgList = [
     "https://media.discordapp.net/attachments/1083303057544200272/1116713534156443718/8v2.png?width=1194&height=682",
     "https://media.discordapp.net/attachments/1083303057544200272/1116708462680883271/A3.png?width=1194&height=682",
-    "http://127.0.0.1:5501/mobil/images/recommended4.jpg",
   ];
   var infoHeaderList = [
     "Burada 1. kilim gösteriliyor",
@@ -16,39 +34,28 @@ function changeDiv(i) {
     "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
     "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
   ];
-  var textureList = [
-    "https://cdn.glitch.global/40abd77b-70dd-401e-b21f-67d60e76f8de/cp1.glb",
-    "https://cdn.glitch.global/40abd77b-70dd-401e-b21f-67d60e76f8de/cp2.glb",
-    "https://cdn.glitch.global/40abd77b-70dd-401e-b21f-67d60e76f8de/cp3.glb",
-  ];
 
   if ((listPage.style.display = "unset")) {
     var image = document.getElementById("infoImage");
     var header = document.getElementById("infoTextHeader");
-    var text = document.getElementById("infoText");    
+    var text = document.getElementById("infoText");
     infoPage.style.display = "unset";
     listPage.style.display = "none";
     image.src = imgList[i];
     header.innerHTML = infoHeaderList[i];
     text.innerHTML = infoTextList[i];
-       
+
   }
 }
 function changeDivBack() {
   var listPage = document.getElementById("bol1");
   var infoPage = document.getElementById("bol2");
   infoPage.style.display = "none";
-  listPage.style.display = "unset";
+  listPage.style.display = "unset";  
 }
 
-setTimeout(function runAR(){
-  var modelvw = document.querySelector("model-viewer#kilim");
-  modelvw.src = "https://cdn.glitch.global/61b3137b-3e3b-4c2c-a6e7-9d3dac7f65fa/SM_Carpet.glb?v=1689007314768"
-  console.log(modelvw); 
-  var model = modelvw.model;
-  console.log(model);
-  var material = model.materials[0];
-  console.log(material);
+function runAR() {
   var but = document.getElementById("arBut");
   but.click();
-},5000);
+  
+}
