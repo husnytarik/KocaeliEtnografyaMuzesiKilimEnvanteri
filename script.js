5e1778087b6477872617b84a4bff79f06fe271cc
Glitch (glitch-hello-website) <none>
2023-07-14 08:33:09 +0000

    👕🌁 Checkpoint ./.glitch-assets:50750112/3599 ./index.html:50750112/409 ./script.js:50750112/313

--- script.js
+++ script.js
@@ -74,13 +74,13 @@ function changeDiv(i) {
   var infoPage = document.getElementById("bol2");
   var textureChange = document.getElementById("texture");
   var imgList = [
-    "https://cdn.glitch.global/61b3137b-3e3b-4c2c-a6e7-9d3dac7f65fa/1.jpg",
-    "https://cdn.glitch.global/61b3137b-3e3b-4c2c-a6e7-9d3dac7f65fa/2.jpg",
-    "https://cdn.glitch.global/61b3137b-3e3b-4c2c-a6e7-9d3dac7f65fa/3.jpg",
-    "https://cdn.glitch.global/61b3137b-3e3b-4c2c-a6e7-9d3dac7f65fa/4.jpg",
-    "https://cdn.glitch.global/61b3137b-3e3b-4c2c-a6e7-9d3dac7f65fa/5.jpg",
-    "https://cdn.glitch.global/61b3137b-3e3b-4c2c-a6e7-9d3dac7f65fa/6.jpg",
-    "https://cdn.glitch.global/61b3137b-3e3b-4c2c-a6e7-9d3dac7f65fa/7.jpg",    
+    "https://cdn.glitch.global/61b3137b-3e3b-4c2c-a6e7-9d3dac7f65fa/a1.jpg",
+    "https://cdn.glitch.global/61b3137b-3e3b-4c2c-a6e7-9d3dac7f65fa/a2.jpg",
+    "https://cdn.glitch.global/61b3137b-3e3b-4c2c-a6e7-9d3dac7f65fa/a3.jpg",
+    "https://cdn.glitch.global/61b3137b-3e3b-4c2c-a6e7-9d3dac7f65fa/a4.jpg",
+    "https://cdn.glitch.global/61b3137b-3e3b-4c2c-a6e7-9d3dac7f65fa/a5.jpg",
+    "https://cdn.glitch.global/61b3137b-3e3b-4c2c-a6e7-9d3dac7f65fa/a6.jpg",
+    "https://cdn.glitch.global/61b3137b-3e3b-4c2c-a6e7-9d3dac7f65fa/a7.jpg",    
   ];
   var infoHeaderList = [
     "Burada 1. kilim gösteriliyor",
